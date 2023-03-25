e8676c2ee384635fdad45dae37252fae4070c550
Oz996 <114649736+Oz996@users.noreply.github.com>
2023-03-25 13:44:08 +0100

    La till path till frontend mappen

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'frontend')))
 
 // app.use(express.json())
 // app.use(express.urlencoded({ extended: false }))
